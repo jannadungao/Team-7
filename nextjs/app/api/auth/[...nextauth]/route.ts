7755767c86421ea60c1e15b69a6b6953b12accb4
Iwantexpresso <99292736+Iwantexpresso@users.noreply.github.com>
2026-02-26 20:49:37 -0600

    added token saving to jwt fornat and its dependent types

--- nextjs/app/api/auth/[...nextauth]/route.ts
+++ nextjs/app/api/auth/[...nextauth]/route.ts
@@ -28,6 +28,21 @@ const handler = NextAuth({
   pages: {
     signIn: "/sign-in",
   },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.accesTokenExpires = account.expires_at;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      session.error = token.error;
+      return session;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
--- nextjs/app/types.ts
+++ nextjs/app/types.ts
@@ -1,14 +1,32 @@
 import { UUID } from "crypto";
 
 export interface User {
-    username: string; // UUID
-    user_id: string; // UUID
-    email: string; // chars@[sub.]example.tld
+  username: string; // UUID
+  user_id: string; // UUID
+  email: string; // chars@[sub.]example.tld
 }
 
 export interface FlexibleTask {
-    user_id: string; //UUID
-    task_id: string; // UUID
-    name: string;
-    amt_mins: number; // INT
+  user_id: string; //UUID
+  task_id: string; // UUID
+  name: string;
+  amt_mins: number; // INT
 }
+
+// next auth types
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string;
+    error?: string;
+  }
+}
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string;
+    refreshToken?: string;
+    accesTokenExpires?: number;
+    error?: string;
+  }
+}
+
+// Calendar API types
