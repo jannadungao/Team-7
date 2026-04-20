b5f77745ef1a33e2208e42ce81de808a76b2f5ff
jannadungao <jannadungao2@gmail.com>
2026-04-19 21:20:26 -0500

    Update navbar.tsx

--- nextjs/components/layout/navbar.tsx
+++ nextjs/components/layout/navbar.tsx
@@ -126,7 +126,7 @@ export default function Example() {
               transition
               className="absolute right-0 z-10 mt-2 origin-top-right rounded-md bg-gray-800 py-1 outline -outline-offset-1 outline-white/10 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
             >
-              {/* redirect to dashboard page */}
+              {/* redirect to setting page */}
                {/* <MenuItem>
                   <a
                   key="Settings"
