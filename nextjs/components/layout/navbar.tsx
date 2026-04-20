1d0a85db093609165e4a5ebb7461272037adda50
jannadungao <jannadungao2@gmail.com>
2026-04-19 20:43:35 -0500

    Update navbar.tsx

--- nextjs/components/layout/navbar.tsx
+++ nextjs/components/layout/navbar.tsx
@@ -127,10 +127,10 @@ export default function Example() {
               className="absolute right-0 z-10 mt-2 origin-top-right rounded-md bg-gray-800 py-1 outline -outline-offset-1 outline-white/10 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
             >
               {/* redirect to dashboard page */}
-               <MenuItem>
+               {/* <MenuItem>
                   <a
-                  key="Dashboard"
-                  href="/dashboard"
+                  key="Settings"
+                  href="/settings"
                   aria-current={"/dashboard" === pathname ? "page" : undefined}
                   className={classNames(
                     "/dashboard" === pathname
@@ -139,9 +139,9 @@ export default function Example() {
                     "block px-4 py-2 items-center text-sm w-full text-gray-300 data-focus:bg-white/5 data-focus:outline-hidden",
                   )}
                 >
-                  Dashboard
+                  Settings
                 </a>
-              </MenuItem>
+              </MenuItem> */}
               <MenuItem>
                   <a
                   key="Mascot Options"
