25251144a89b0114acd3221c2bc7bb3b7eb33860
Addie Bart <addiebart@ku.edu>
2026-04-26 18:47:51 -0500

    settings header is now horizontal

--- nextjs/app/settings/page.tsx
+++ nextjs/app/settings/page.tsx
@@ -9,8 +9,9 @@ import MascotSelect from "@/components/features/settings"
 
 export default function Page() {
     return (
-        <>  <header className="relative bg-gray-800 after:pointer-events-none after:absolute after:inset-x-0 after:inset-y-0 after:border-y after:border-white/10">
-                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <div>  
+            <header className="relative bg-gray-800 after:pointer-events-none after:absolute after:inset-x-0 after:inset-y-0 after:border-y after:border-white/10">
+                <div className="px-4 py-6 sm:px-6 lg:px-8">
                     <h1 className="text-2xl font-semibold tracking-tight text-white">Settings</h1>
                 </div>
             </header>
@@ -19,7 +20,8 @@ export default function Page() {
                 <br /><hr /><br />
                 <h2>Appearance</h2>
 
+
             </div>
-        </>
+        </div>
     )
 }
