1fb1074aa40073e98583314002f55033b65623c6
jannadungao <jannadungao2@gmail.com>
2026-04-26 18:21:38 -0500

    Settings page renaming

--- nextjs/app/settings/page.tsx
+++ nextjs/app/settings/page.tsx
@@ -5,13 +5,13 @@
  * Author(s): Janna Dungao
  * Date: 03/25/26
  */
-import MascotSelect from "@/components/features/mascotSelect"
+import MascotSelect from "@/components/features/settings"
 
 export default function Page() {
     return (
         <>  <header className="relative bg-gray-800 after:pointer-events-none after:absolute after:inset-x-0 after:inset-y-0 after:border-y after:border-white/10">
                 <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <h1 className="text-2xl font-semibold tracking-tight text-white">Mascot Dashboard</h1>
+                    <h1 className="text-2xl font-semibold tracking-tight text-white">Settings</h1>
                 </div>
             </header>
             <div className="p-4">
