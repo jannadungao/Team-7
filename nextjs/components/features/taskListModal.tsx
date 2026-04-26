7a2acdd5aa3e288a2fd113b1473fb4b87b426ee8
Addie Bart <addiebart@ku.edu>
2026-04-26 15:16:31 -0500

    small fix regarding svg attribute name in JSX

--- nextjs/components/features/taskListModal.tsx
+++ nextjs/components/features/taskListModal.tsx
@@ -252,7 +252,7 @@ export default function TaskListModal({buttonStyles, forcedCategory} : {buttonSt
                         >   
                             {/* Button to close modal */}
                             <button type="button" onClick={() => setOpen(false)} className="absolute top-0 right-0 p-4 cursor-pointer">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                                 </svg>
                             </button> 
